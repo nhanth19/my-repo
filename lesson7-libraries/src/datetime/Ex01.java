e408c79967e776cefffd6a2426771906e1208bd0
vo nhan <vothanhnhan270@gmail.com>
2023-02-28 17:20:06 +0700

    learning..

---
+++ lesson7-libraries/src/datetime/Ex01.java
@@ -0,0 +1,49 @@
+package datetime;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Ex01 {
+	private static String[] months = {"Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"};
+	
+	public static void main(String[] args) {
+		//timezone : depend locale
+		//Date = new Date() : khai bao nhieu lan,ton memory
+		//Calendar.getIntance(): singleton pattern
+		//ICT : indochina time
+		//epoch time: 1/1/1970 : moc thoi gian dc set tren thiet bi dien tu, de tinh milis
+		
+		Date date = new Date();
+		System.out.println("date : " + date);
+		
+		Calendar c = Calendar.getInstance();
+		
+		//set time
+//		c.set(Calendar.MONTH, 0);
+		System.out.println("calendar :" + c);
+
+		int day = c.get(Calendar.DAY_OF_MONTH);
+		int month = c.get(Calendar.MONTH);
+		int year = c.get(Calendar.YEAR);
+		System.out.println("d/m/y : " + day + "/" + months[month] + "/" + year);
+		
+		int hour = c.get(Calendar.HOUR);         //hour12
+		int tag = c.get(Calendar.AM_PM);
+		int hour2 = c.get(Calendar.HOUR_OF_DAY); //hour24
+		int minute = c.get(Calendar.MINUTE);
+		int second = c.get(Calendar.SECOND);
+		System.out.println("h:m:s : " + hour + ":" + minute + ":"+ second);
+		
+		//number of days in month, year
+		int daysOfMonthActual = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int daysOfMonthAlway = c.getMaximum(Calendar.DAY_OF_MONTH);
+		System.out.println("daysOfMonthActual : " + daysOfMonthActual);
+		System.out.println("daysOfMonthAlway : " + daysOfMonthAlway);
+		
+		int daysOfYear = c.getActualMaximum(Calendar.DAY_OF_YEAR);
+		System.out.println("daysOfYear : " + daysOfYear);
+		
+		//first day of week : depend locale
+		int firstDayOfWeek = c.getFirst
+	}
+}
